3cab7b49e987e5e642e22629dd6467071afd0d9e
AkshithKachaka1 <67799241+Akshith-coder@users.noreply.github.com>
2021-07-24 16:37:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+NumList = [6, 4, 9, 3, 1]
+Number = int(len(NumList))
+
+for i in range (Number):
+    for j in range(i + 1, Number):
+        if(NumList[i] > NumList[j]):
+            temp = NumList[i]
+            NumList[i] = NumList[j]
+            NumList[j] = temp
+
+print("Element After Sorting List in Ascending Order is : ", NumList)
